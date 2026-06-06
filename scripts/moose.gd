837ea3e408c4911db6173f523c86a97f43d5f7a9
maksutheam <60304828+maksutheam@users.noreply.github.com>
2026-06-06 13:54:40 +0300

    Updated moose charge

--- scripts/moose.gd
+++ scripts/moose.gd
@@ -16,13 +16,22 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_SPACE):
-		moose_charge(Vector3(20, 20, 0))
+func _process(_delta: float) -> void:
+	# Charge on press if debug
+	if Input.is_action_just_pressed("ui_accept") and OS.is_debug_build():
+		var rand_x := randf_range(-30, 30)
+		var rand_z := randf_range(-30, 30)
+		moose_charge(Vector3(rand_x, 0, rand_z))
 
 
+func moose_warning():
+	pass
+
+
+## Makes the Moose charge to ´where´.
 func moose_charge(where: Vector3):
-	position = lerp(position, where, .01)
+	var charge_tween := get_tree().create_tween()
+	charge_tween.tween_property(self, "global_position", where, charge_time)
 
 
 func _on_moose_charge_timer_timeout() -> void:
