48d0e97e2ce405b33c65a478b66350f2a24f3016
maksutheam <60304828+maksutheam@users.noreply.github.com>
2026-06-06 15:03:53 +0300

    Moose charges past player

--- scripts/moose.gd
+++ scripts/moose.gd
@@ -3,6 +3,7 @@ extends Node3D
 
 enum States{IDLE, NOTICE, CHARGE}
 
+@export var player: Player
 @export_category("Charge")
 @export var charge_time := 5.0
 @export var charge_delay_min := 5.0
@@ -31,8 +32,21 @@ func moose_warning():
 ## Makes the Moose charge to ´where´.
 func moose_charge(where: Vector3):
 	var charge_tween := get_tree().create_tween()
+	where = Vector3(where.x, 0, where.z)
 	charge_tween.tween_property(self, "global_position", where, charge_time)
 
 
+# TODO make this warn and charge toward the player
+## Makes the moose charge when the timer runs out
 func _on_moose_charge_timer_timeout() -> void:
+	if player == null:
+		var rand_x := randf_range(-30, 30)
+		var rand_z := randf_range(-30, 30)
+		moose_charge(Vector3(rand_x, 0, rand_z))
+		push_warning("player variable hasn't been set in Moose")
+	else:
+		var charge_distance := global_position.distance_to(player.global_position)
+		var charge_dir := global_position.direction_to(player.global_position)
+		var charge_pos := charge_dir * (charge_distance * 2)
+		moose_charge(charge_pos)
 	%MooseChargeTimer.start(randf_range(charge_delay_min, charge_delay_max))
