225b6cae3e4fa32244a00a81f13afa8353e1c12f
maksutheam <60304828+maksutheam@users.noreply.github.com>
2026-06-07 13:18:28 +0300

    add sign sprite

--- scripts/moose_mark.gd
+++ scripts/moose_mark.gd
@@ -11,5 +11,5 @@ func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body is Player and not marked:
 		marked = true
 		%Mark.hide()
-		%Sign.show()
+		%SignSprite.show()
 		area_marked.emit()
